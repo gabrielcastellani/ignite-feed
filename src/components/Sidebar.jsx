0d98e20c0a83d8a60cd07f62b19ad69aabe98a31
gabrielcastellani <gabrielcastellanioliveira@gmail.com>
2022-07-11 21:13:48 -0300

    Finished sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,23 @@
 import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <img className={styles.cover} src="https://images.unsplash.com/photo-1657485238475-79f14c891905?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" />
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1657485238475-79f14c891905?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"
+            />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/gabrielcastellani.png" />
                 <strong>Gabriel Castellani</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     );
